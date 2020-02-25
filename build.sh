f782a7c13da22d7c37bb6846ae6993a7496bcbb2
juandelperal <juandelperal@gmail.com>
2020-02-25 10:27:02 -0300

    updated libs

--- build.sh
+++ build.sh
@@ -1,29 +1,22 @@
 #!/bin/sh
-
 files=(PiazzollaVARsetup PiazzollaVARsetupItalic)
 # files=(Piazzolla PiazzollaItalic)
 
 for f in "${files[@]}"; do
-    echo
     echo Setup DesignSpace from Glyphs
     if [ -e temp/building/$f ]; then rm -rf temp/building/$f; fi
     mkdir -p temp/building/$f
     glyphs2ufo sources/$f.glyphs -m temp/building/$f
-    echo
     echo Process DesignSpace
     python processDesignSpace.py $f
 done
 
 for f in "${files[@]}"; do
-    echo
     echo "Generate variable fonts for $f"
     fontmake -m temp/building/$f/$f.designspace -o variable --output-dir fonts/variable --verbose WARNING
-    # echo
-    # echo "Generate static fonts for $f"
+        # echo "Generate static fonts for $f"
     # fontmake -m sources/$f.designspace -i --output-dir fonts/static --verbose WARNING
 done
-
-echo
 echo Fix fonts
 for VF in fonts/variable/*.ttf; do
     gftools fix-dsig -f $VF
@@ -50,20 +43,14 @@ done
 #     mv "$otf.fix" $otf
 # done
 
-# echo
 # echo Check sources
 # mkdir -p tests
 # cd tests
 # fontbakery check-ufo-sources --ghmarkdown ufo-report.md ../sources/*
-# echo
 # echo Check variable fonts
 # fontbakery check-universal --ghmarkdown variable-report.md ../fonts/variable/*
-# echo
 # echo Check static ttfs
 # fontbakery check-universal --ghmarkdown ttfs-report.md ../instance_ttf/*
-# echo
 # echo Check static otfs
-# fontbakery check-universal --ghmarkdown otfs-report.md ../instance_otf/*
-echo
-echo Order files
+# fontbakery check-universal --ghmarkdown otfs-report.md ../instance_otf/*echo Order files
 cp extra/Thanks.png fonts
