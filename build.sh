ba150efef4ede2aa808f72e7200f8114e9efd1d3
juandelperal <juandelperal@gmail.com>
2020-08-27 12:25:16 +0200

    fixNameTable.py removed from chain

--- build.sh
+++ build.sh
@@ -62,7 +62,6 @@ done
 
 if $static; then
     for ttf in fonts/Piazzolla/static/*.ttf; do
-        python tools/fixNameTable.py $ttf
         gftools fix-dsig -f $ttf
         gftools fix-nonhinting $ttf "$ttf.fix"
         mv "$ttf.fix" $ttf
@@ -76,7 +75,6 @@ if $static; then
     done
 
     for otf in fonts/Piazzolla/static/*.otf; do
-        python tools/fixNameTable.py $VF
         gftools fix-weightclass $otf
         gftools fix-dsig -f $otf
         gftools fix-nonhinting $otf "$otf.fix"
